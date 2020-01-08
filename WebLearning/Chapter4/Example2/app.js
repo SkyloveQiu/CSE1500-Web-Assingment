d87c0d0db8c727fda16486c8631c070c710c7292
邱子昂 <skylove@wlan-145-94-252-138.wlan.tudelft.nl>
2020-01-08 20:33:02 +0100

    Chapter4

--- WebLearning/Chapter4/Example2/app.js
+++ WebLearning/Chapter4/Example2/app.js
@@ -1,5 +1,17 @@
-var main = function () { "use strict";
-$(".comment-input button").on("click", function (event) { console.log("Hello World!");
-});
- };
-$(document).ready(main)
+var main = function () {
+    "use strict";
+    $(".comment-input button").on("click",function(event) {
+        
+
+        if($(".comment-input input").val() !== ""){
+            var $new_comment = $("<p>"),
+            comment_text = $(".comment-input input").val();
+            $new_comment.text(comment_text);     
+            $(".comment").append($new_comment);
+        }
+    });
+};
+
+
+$(document).ready(main);
+
