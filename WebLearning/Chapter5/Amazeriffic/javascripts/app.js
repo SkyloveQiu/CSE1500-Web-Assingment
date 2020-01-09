12942f3efd9aea9c397c9b0fb65673b6d791a08f
邱子昂 <skylove@wlan-145-94-253-213.wlan.tudelft.nl>
2020-01-09 21:32:00 +0100

    learning

--- WebLearning/Chapter5/Amazeriffic/javascripts/app.js
+++ WebLearning/Chapter5/Amazeriffic/javascripts/app.js
@@ -1,49 +1,95 @@
 var main = function(toDoObjects) {
     "use strict";
-
-    var toDos = toDoObjects.map(function(toDo) {
+    var toDos = toDoObjects.map(function (toDo) {
+        // we'll just return the description
+        // of this toDoObject
         return toDo.description;
     });
-
-    console.log(toDos);
-
-
 
     $(".tabs a span").toArray().forEach(function(element) {
         $(element).on("click",function(){
             var $element = $(element);
-            var $content,$input,$button;
+            var $content;
             $(".tabs a span").removeClass("active");
             $element.addClass("active");
             $("main .content").empty();
 
             if($element.parent().is(":nth-child(1)")) {
-                console.log("First tab clicked!");
+                $content = $("<ul>");
+                toDos.forEach(function(todo) {
+                    $content.append($("<li>").text(todo));
+                });
             } else if ($element.parent().is(":nth-child(2)")) {
                 $content = $("<ul>");
                 toDos.forEach(function(todo) {
                     $content.append($("<li>").text(todo));
                 });
-                $("main .content").hide();
-                $("main .content").append($content);
-                $("main .content").fadeIn();
+                
                 
             } else if ($element.parent().is(":nth-child(3)")) {
-                console.log("Third tab clicked!");
-            } else if ($element.parent().is(":nth-child(4)")) {
-                $input = $("<input>");
-                $button = $("<button>").text("+");
-                $button.on("click",function(){
-                    toDos.push($input.val());
-                    $input.val("");
+                var tags = [];
+
+                toDoObjects.forEach(function (toDo) {
+                    toDo.tags.forEach(function (tag) {
+                        if (tags.indexOf(tag) === -1) {
+                            tags.push(tag);
+                        }
+                    });
+                });
+                console.log(tags);
+
+                var tagObjects = tags.map(function (tag) {
+                    var toDosWithTag = [];
+
+                    toDoObjects.forEach(function (toDo) {
+                        if (toDo.tags.indexOf(tag) !== -1) {
+                            toDosWithTag.push(toDo.description);
+                        }
+                    });
+
+                    return { "name": tag, "toDos": toDosWithTag };
                 });
 
-                $content = $(".content").append($input).append($button);
+                tagObjects.forEach(function (tag) {
+                    var $tagName = $("<h3>").text(tag.name),
+                        $content = $("<ul>");
+
+
+                    tag.toDos.forEach(function (description) {
+                        var $li = $("<li>").text(description);
+                        $content.append($li);
+                    });
+
+                    $("main .content").append($tagName);
+                    $("main .content").append($content);
+                });
+            } else if ($element.parent().is(":nth-child(4)")) {
+                var $input = $("<input>").addClass("description");
+                var $button = $("<button>").text("+");
+                var $inputLabel = $("<p>").text("Description: ");
+                var $tagInput = $("<input>").addClass("tags");
+                var $tagLabel = $("<p>").text("Tags: ");
+                $button.on("click",function(){
+                    var description = $input.val();
+                    var tags = $tagInput.val().split(",");
+
+                    toDoObjects.push({"description":description, "tags":tags});
+
+                    toDos = toDoObjects.map(function (toDo) {
+                        return toDo.description;
+                    })
+                    $input.val("");
+                    $tagInput.val("");
+                });
+
+                $content = $("<div>").append($inputLabel).append($input).append($tagLabel).append($tagInput).append($button);
             }
+            $("main .content").append($content);
 
             return false;
         });
-        
+        $(".tabs a:first-child span").trigger("click");
+
     });
 }
 
